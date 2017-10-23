f96c8a0ce01d2e7b38da30c24c03f2fcf0a1bb5d
Ngamsiri Buaman <ngamsiri.bm@gmail.com>
2017-10-23 21:58:33 +0700

    ConvertToBinary

--- sim.py
+++ sim.py
@@ -3,6 +3,7 @@
 def main():
         lines = []
         a = 0
+        mem = []
         print('@@@\nstate:')
        # print('\tpc %d' )
         print('\tmemory:')
@@ -11,13 +12,27 @@ def main():
                         for line in out:
                                 line = line.strip()
                                 print('\t\tmem[%d]: %s' %(a,line))
+                                line = int(str(line))
+                                Bin = bin(line)
+                                #D = '{0:025b}'.format(Bin)
+                                #print(Bin)        
+                                Opcode = str(Bin[0:3])
+                                print(Opcode)   
                                 a += 1
                 print('\tregisters:')
                 for b in range(0,8):
                         print('\t\treg[%d]: 0' %b)
-                print('End state\n@@@')    
+                print('End state\n')    
 
-def 
-                
+#def
+
+def FillBin(Bin):
+                Bin25 = '{0:025b}'.format(Bin)
+                return Bin25
+      
+
+    
+               
+
 if __name__ == "__main__":
     main()
