92bae4efe850e271f091c40b52ac33607a408aed
Uttakran Renuman <qowieury_kidmaioak@hotmail.com>
2017-11-18 00:12:33 +0700

    fix jalr that not save correctly

--- simulator.py
+++ simulator.py
@@ -10,24 +10,24 @@ def printState(reg,pc,lines):
     for b in range(0, 8):
         print('\t\treg[ %d ] ' % b, reg[b])
     print('end state')
-     
+
 def main():
 
     lines = []
     reg = [0, 0, 0, 0, 0, 0, 0, 0]
-    with open('allinstruct.out','r') as out:
+    with open('myfibo.out','r') as out:
         for line in out:
             line = line.strip()
             lines.append(line)
 
     count = 0
     pc = 0
-    inst = 0 
-    
+    inst = 0
+
     while (pc >= 0 ):
         DectoBin = ToBin(int(lines[inst]))
         OP = str(DectoBin)[0:3]
-                 
+
 
 #add
         if OP == '000':
@@ -36,15 +36,15 @@ def main():
             regB = int((DectoBin)[6:9],2)
             Bit15_3 = int((DectoBin)[9:22],2)
             destReg = int((DectoBin)[22:25],2)
-  
+
             add = reg[regA] + reg[regB]
             value = '{0:b}'.format(add)
 
-            if len(value) > 32: #checks bit over 32 
+            if len(value) > 32: #checks bit over 32
                 value = value[len(value)-32:]
-                
+
             reg[destReg] = int ((value),2)
-            
+
             inst+=1
             pc+=1
             count+=1
@@ -66,7 +66,7 @@ def main():
                 newregA = regA
                 regAA = '{0:b}'.format(newregA)
                 regA1 = incrBit0(regAA,len(regAA))
-                
+
             if regB < 0 :
                 newregB = regB*(-1)
                 regBB = '{0:b}'.format(newregB)
@@ -82,8 +82,8 @@ def main():
                     destReg += '0'
                 else:
                     destReg += '1'
-            
-            if destReg[0] =='1': #convert to decimal 
+
+            if destReg[0] =='1': #convert to decimal
                 bww = int(bwBit(destReg),2)+1
                 dec = bww*(-1)
             else:
@@ -93,16 +93,16 @@ def main():
             inst+=1
             pc+=1
             count+=1
-            
+
 #lw
         elif (OP == '010'):
             printState(reg,pc,lines)
             regA = int((DectoBin)[3:6],2)
             regB = int((DectoBin)[6:9],2)
             offset = (DectoBin)[9:25]
-            
+
             offsetfield = two_cmp(offset) #2's complement
-            memA = offsetfield + reg[regA] 
+            memA = offsetfield + reg[regA]
             reg[regB] = int(lines[memA]) #Value of memory to registerB
 
             inst+=1
@@ -115,24 +115,24 @@ def main():
             regA = int((DectoBin)[3:6],2)
             regB = int((DectoBin)[6:9],2)
             offset = (DectoBin)[9:25]
-            
+
             offsetfield = two_cmp(offset)#2's complement
-            memA = offsetfield + reg[regA] 
-        
-            if memA > len(lines): #Stack case 
+            memA = offsetfield + reg[regA]
+
+            if memA > len(lines): #Stack case
                 lines.append(reg[regB])
             else:
                 lines[memA] = (reg[regB]) #Value of registerB to memory
-                
+
             pc+=1
             inst+=1
             count+=1
-  
-            
+
+
 #beq
         elif OP == '100':
             printState(reg,pc,lines)
-            beq_pc = -1 #position of brach always -1 
+            beq_pc = -1 #position of brach always -1
             regA = int((DectoBin)[3:6],2)
             regB = int((DectoBin)[6:9],2)
             offset = (DectoBin)[9:25]
@@ -140,7 +140,7 @@ def main():
             offsetfield = two_cmp(offset) #2's complement
             regA = reg[regA]
             regB = reg[regB]
-            if regA == regB : #check condition of beq. instruction 
+            if regA == regB : #check condition of beq. instruction
                 addr = (beq_pc+1)+offsetfield
                 inst = inst + addr +1
                 pc = lines.index(lines[inst])
@@ -148,9 +148,9 @@ def main():
                 inst+=1
                 pc+=1
             count+=1
-            
-                
-                
+
+
+
 #jalr
         elif OP == '101':
             printState(reg,pc,lines)
@@ -158,14 +158,16 @@ def main():
             regA = int((DectoBin)[3:6],2)
             regB = int((DectoBin)[6:9],2)
             Bit15_0 = int((DectoBin)[9:25],2)
-            
-            addrRegB = pc+1 
+
+            addrRegB = pc+1
             addr = reg[regA]
 
             if regA == regB: #check condition of jalr. instruction
+                reg[regB] = addrRegB
                 pc = addrRegB
                 inst = pc
             else:
+                reg[regB] = addrRegB
                 pc = addr
                 inst = pc
             count+=1
@@ -186,7 +188,7 @@ def main():
         elif OP == '111' : #do nothing
             Bit21_0 = int((DectoBin)[3:25],2)
             count+=1
-            
+
         else:
             fill = DectoBin
 
@@ -218,7 +220,7 @@ def two_cmp(offset):
     else:
         dec = int(bin32,2)
     return dec
-    
+
 #-------------- Backward bit---------------------
 def bwBit(bitBin):
     bwBit = ''
@@ -226,28 +228,28 @@ def bwBit(bitBin):
             if bitBin[i] == '1':
                 bwBit += '0'
             else:
-                bwBit += '1'        
+                bwBit += '1'
     return bwBit
 
 #---------For nand: Negative Value-------------
 def bwBitBin(reg):
     incrr = ''
     bwB = ''
-    for i in range(0,len(reg)): 
+    for i in range(0,len(reg)):
         if reg[i] == '1':
             bwB += '0'
         else:
             bwB += '1'
-    bw = int(bwB,2)+1 #backward bit(dec)+1 
+    bw = int(bwB,2)+1 #backward bit(dec)+1
     bw  = '{0:0b}'.format(bw) #change backward bit(dec) to Binary
     s = len(reg)
-    for m in range(0,s-len(bw)): #Add bit 
+    for m in range(0,s-len(bw)): #Add bit
         incrr += '0'
     bw = incrr + bw
     regA1 = incrBit1(bw,s)
     return regA1
 
-#---------For nand: add bit sign extend--------- 
+#---------For nand: add bit sign extend---------
 def incrBit1(bit,s):
     incr = ''
     for n in range(0,32-s):
@@ -255,14 +257,13 @@ def incrBit1(bit,s):
     reg = incr + bit
     return reg
 
-#---------For nand: add bit sign extend--------- 
+#---------For nand: add bit sign extend---------
 def incrBit0(bit,s):
     incr = ''
     for n in range(0,32-s):
         incr += '0'  #when positive value ,add '0'
     reg = incr + bit
     return reg
-    
+
 if __name__ == "__main__":
     main()
-
